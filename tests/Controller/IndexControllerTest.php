0a694d6b5916e05472bf2fb3dbc01031b34e56ef
Sébastien Attal <s.attal@bilendi.com>
2021-01-06 22:19:58 +0100

    Create first page

---
+++ tests/Controller/IndexControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class IndexControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+}
